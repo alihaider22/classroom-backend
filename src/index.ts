b73ea1c54a647fe919fe83fc221bf6d1e0f1a90d
Ali Haider <alihaider@foomotion.io>
2026-02-02 11:51:41 +0500

    setup db and tables

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import express from 'express';
 const app = express();
 const port = 8000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello from the Express server!');
 });
